f9c2a08ade60fc2a732f8362f7c3e23be487800d
MDLymh <luhernandez@grupozeta.com>
2024-10-03 09:31:01 -0600

    Creacion de estructura de componentes y modulos de react, ejemplo de modulo cargado en la vista por defecto

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import * as glob from 'glob';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
+        [react()],
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [...glob.sync('resources/css/**/*.css'), ...glob.sync('resources/js/**/*.js'),...glob.sync('resources/js/**/*.jsx')],
             refresh: true,
         }),
+
     ],
 });
