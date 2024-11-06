1a43fd1dd051ce5c1dc9f665175a5cfe1a58245d
Carlos Samuel <87548939+carlossamuelgonzalezjasso@users.noreply.github.com>
2024-11-06 00:05:37 -0600

    added sample

---
+++ resources/jsx/modelos/csrf/csrf.jsx
@@ -0,0 +1,11 @@
+import "./csrf.css"
+
+export const Csrf = ()=>{
+    let csrf = document.querySelector("meta[name='csrf']").getAttribute('content');
+    return (
+        <>
+            <input type="hidden" name="_token" value={csrf} autocomplete="off"></input>
+        </>
+    );
+    
+}
