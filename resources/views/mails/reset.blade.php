a4068e6d8e2a2ce612db826b6f45ac6931cc5e61
Carlos Samuel <87548939+carlossamuelgonzalezjasso@users.noreply.github.com>
2024-11-06 00:11:22 -0600

    Create reset.blade

---
+++ resources/views/mails/reset.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Reset de usuario</title>
+</head>
+<body>
+    <h1>Reinicio de contraseña</h1>
+    <p>Hola buen dia, </p>
+    <p>Hemos recibido una solicitud de reinicio de contraseña si no lo haz solicitado has caso omiso </p>
+</body>
+</html>
